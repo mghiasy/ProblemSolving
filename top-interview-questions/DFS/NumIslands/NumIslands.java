12e4250ab6d3df63f65a5501bec90c4cfb97f3a7
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-08-16 19:50:21 -0500

    Modularize noIslands

--- top-interview-questions/DFS/NumIslands/NumIslands.java
+++ top-interview-questions/DFS/NumIslands/NumIslands.java
@@ -1,20 +1,24 @@
 package LeetCode;
 
 import java.util.Stack;
+//time: o(M*N) => M:no of rows, N:no of cols
+//space o(2N) =>size of stack
 
+//Runtime: 5 ms, faster than 17.87% of Java online submissions for Number of Islands.
+//Memory Usage: 42 MB, less than 76.79% of Java online submissions for Number of Islands.
 public class NumIslands {
     public static int numIslands(char[][] grid) {
         Stack<int[]> st= new Stack<>();
         int x=0,y=0,islandCount=0;
         int[] popItem=new int[2];
-        for(int i=0;i<grid.length;++i){//no od rows
-            for(int j=0;j<grid[0].length;++j){
+        for(int i=0;i<grid.length;++i){//no of rows
+            for(int j=0;j<grid[0].length;++j){//no of cols
                 if(grid[i][j]=='1'){
                     st.push(new int[]{i,j});
                     grid[i][j]='0';
                     islandCount++;
                 }
-                while (!st.isEmpty()){
+                while (!st.isEmpty()){//will executed to the (number of 1)/4, worse case all of them are 1
                     popItem =st.pop();
                     x=popItem[0];
                     y=popItem[1];
@@ -38,6 +42,40 @@ public class NumIslands {
             }
         }
         return islandCount;
+    }
+
+    //modularize
+    //Runtime: 4 ms, faster than 25.59% of Java online submissions for Number of Islands.
+    //Memory Usage: 41.5 MB, less than 98.71% of Java online submissions for Number of Islands.
+    public static int numIslands2(char[][] grid) {
+        Stack<int[]> st= new Stack<>();
+        int x=0,y=0,islandCount=0;
+        int[] popItem=new int[2];
+        for(int i=0;i<grid.length;++i){//no od rows
+            for(int j=0;j<grid[0].length;++j){
+                if(grid[i][j]=='1'){
+                    st.push(new int[]{i,j});
+                    grid[i][j]='0';
+                    islandCount++;
+                }
+                while (!st.isEmpty()){
+                    popItem =st.pop();
+                    x=popItem[0];
+                    y=popItem[1];
+                    helperMethod(grid,st,x+1,y);
+                    helperMethod(grid,st,x-1,y);
+                    helperMethod(grid,st,x,y+1);
+                    helperMethod(grid,st,x,y-1);
+                }
+            }
+        }
+        return islandCount;
+    }
+    public static void helperMethod(char[][] grid,Stack<int[]> st,int x,int y){
+        if(x<grid.length && y<grid[0].length && x>=0 && y>=0 && grid[x][y]=='1') {
+            st.push(new int[]{x, y});
+            grid[x][y] = '0';
+        }
     }
 
     public static void main(String[] args) {
