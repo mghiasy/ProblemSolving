3311302b81f52eb61c5847dfa1b040d059d475c7
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-08-22 21:19:42 -0500

    LadderLength

---
+++ ServiceNow/ProductExceptSelf/ProductExceptSelf.java
@@ -0,0 +1,11 @@
+package serviceNow;
+
+public class ProductExceptSelf {
+    //with division
+    //loop over array and calc product of all = product
+    //loop over array and put newArr[i]=product/arr[i]
+    //o(n)
+    public int[] productExceptSelf(int[] nums) {
+        return null;
+    }
+}
