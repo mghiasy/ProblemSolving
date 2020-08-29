334d34b82337e1418c1e14483ec63a11be869c29
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-08-29 02:13:54 -0500

    Add Score rank sol2

--- Database/RankScores/Solution.sql
+++ Database/RankScores/Solution.sql
@@ -1,8 +1,15 @@
 -- This function will assign rank to each row within a partition without gaps
 Select Score, DENSE_RANK() over(order by Score desc) as drnk
-from LeetCode.Scores
-
+from LeetCode.Scores;
 
 -- Rank() over(order by Score desc) as rnk
 -- This function will assign rank to each row within a partition with gaps.
 -- may have some gaps between Ranks
+
+-- ??
+SELECT
+  Score,
+  (SELECT count(distinct Score) FROM Scores WHERE Score >= s.Score) 'Rank'
+FROM LeetCode.Scores s
+ORDER BY Score desc;
+
