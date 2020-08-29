22889ad06f68783bc69d97b1ae2ba35dcda3e84e
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-08-29 02:41:57 -0500

    Add Employees Earning More Than Their Managers sol2

--- Database/EmployeesEarnMoreThanManagers/Solution.sql
+++ Database/EmployeesEarnMoreThanManagers/Solution.sql
@@ -1,2 +1,13 @@
+-- Runtime: 862 ms, faster than 11.82% of MySQL online submissions for Employees Earning More Than Their Managers.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Employees Earning More Than Their Managers.
 Select name as emp_Name  from Employee e1
-where Salary> (Select Salary from Employee e2 where Id = e1.ManagerId )
+where Salary> (Select Salary from Employee e2 where Id = e1.ManagerId );
+
+-- Runtime: 695 ms, faster than 24.49% of MySQL online submissions for Employees Earning More Than Their Managers.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Employees Earning More Than Their Managers.
+Select e1.name as emp_name from Employee e1, Employee e2
+where e1.ManagerId = e2.Id and e1.Salary> e2.Salary;
+
+-- Runtime: 406 ms, faster than 59.14% of MySQL online submissions for Employees Earning More Than Their Managers.
+Select e1.name as emp_name from Employee e1 join Employee e2
+on e1.ManagerId = e2.Id and e1.Salary> e2.Salary;
