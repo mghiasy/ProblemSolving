8ecf72a324a854a9032675877271aeac6a9c0801
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-08-31 15:00:19 -0500

    no need to condition for COUNT --> group by on more than 1 item means they should be equal

--- Database/ActorsandDirectors/Solution.sql
+++ Database/ActorsandDirectors/Solution.sql
@@ -10,4 +10,9 @@ from (Select actor_id,director_id , Count(*) as num
 Select actor_id,director_id 
 From ActorDirector
 group by 1,2
-Having Count(IF(1=2 ,1,0))>=3
+Having Count(IF(1=2 ,1,0))>=3;
+
+-- no need to condition for COUNT --> group by on more than 1 item means they should be equal
+select actor_id,director_id from ActorDirector
+group by actor_id,director_id 
+having count(*)>=3
