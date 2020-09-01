822f6a167c6746757b44a6f71c790d7f576df6fa
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-08-31 20:19:36 -0500

    add TwoSum - Tree

--- Database/ConsecutiveNumbers/Solution.sql
+++ Database/ConsecutiveNumbers/Solution.sql
@@ -10,4 +10,4 @@ and L2.Num = l3.Num;
  -- multiple JOIN with multiple conditions
 Select distinct l1.Num as ConsecutiveNums
 From Logs l1 JOIN Logs l2 on l2.Id - l1.id =1 and l1.Num= L2.Num
-JOIN  Logs l3 on l3.Id - l2.id =1 and L2.Num = l3.Num
+JOIN  Logs l3 on l3.Id - l2.id =1 and L2.Num = l3.Num -- SECOND JOIN
